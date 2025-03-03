eb42668226688ad45d6d3ffc1259cb73ecff0e8f
palontologist <gkarimi@kabarak.ac.ke>
2025-03-03 15:04:41 +0300

    Adjust main section vertical padding

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       <FloatingPixels />
       <SoundEffects />
 
-      <main className="max-w-4xl mx-auto px-4 py-8">
+      <main className="max-w-4xl mx-auto px-4 py-4">
           
         <PixelHero />
 
